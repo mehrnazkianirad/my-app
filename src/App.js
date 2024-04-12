b36663bd15f8ffdded5c765f7a0bdd380875073e
mehrnazkianirad <mehrnazkianirad123@gmail.com>
2024-04-12 13:02:12 +0330

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Changes to be committed:
    	new file:   .env
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/App.js
    	new file:   src/component/Home.js
    	new file:   src/component/Login.js
    	new file:   src/component/Logout.js
    	new file:   src/component/Navigations.js
    	modified:   src/index.js
    	new file:   src/interceptor/Axios.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {Login} from "./component/Login";
+import {Home} from "./component/Home";
+import {Navigation} from './component/navigation';
+import {Logout} from './component/Logout';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return 
+      <BrowserRouter>
+        <Navigation></Navigation>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/logout" element={<Logout/>}/>
+        </Routes>
+      </BrowserRouter>;
 }
 
 export default App;
